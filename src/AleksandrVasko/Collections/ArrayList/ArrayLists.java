1147f2d0ceb661f057483a2ba76f237c92b39a2c
Nikolay <nikolai.ordynsky@yandex.ru>
2021-06-13 19:47:45 +0300

    13,06

--- src/AleksandrVasko/Collections/ArrayList/ArrayLists.java
+++ src/AleksandrVasko/Collections/ArrayList/ArrayLists.java
@@ -4,17 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayLists {
-
     private List<String> listTask = new ArrayList<>();
 
     // Добавить строку в конец коллекции List.
-
-    public void addListTask(String task) {
+    public void addInList(String task) {
         listTask.add(task);
     }
 
     //Добавить строку в определенную позицию колеекции List.
-    public void addIndexListTask(int index, String task){
+    public void addInIndexList(int index, String task) {
         listTask.add(index, task);
+    }
+
+    // Получить индекс порядковой позиции строки в коллекции.
+    public int seurchIndexList(String task) {
+        return listTask.indexOf(task);
+    }
+
+    //Изменить строку  в коллекции, в позиции index.
+    public void setList(int index, String task) {
+        listTask.set(index, task);
+    }
+
+    //Удалить строку из коллекции.
+    public void removeList(int index) {
+        listTask.remove(index);
+    }
+
+    //Показать элементы коллекции.
+    public void printList() {
+        for (int i = 0; i < listTask.size(); i++) {
+            System.out.println(i+"["+listTask.get(i)+"]");
+        }
+    }
+    public int sizeList(){
+
+        return listTask.size();
     }
 }
--- src/AleksandrVasko/Collections/Main.java
+++ src/AleksandrVasko/Collections/Main.java
@@ -1,4 +1,95 @@
 package AleksandrVasko.Collections;
 
+import AleksandrVasko.Collections.ArrayList.ArrayLists;
+
+
+import java.util.Scanner;
+
 public class Main {
+    private static void instruction() {
+        System.out.println("""
+                Нажмите :\s
+                1.Добавить задачу в конец коллекции.\s
+                2.Добавить задачу в определенную позицию коллекции.
+                3.Получить индекс порядковой позиции задачи в коллекции.
+                4.Изменить задачу  в коллекции, в указанной позиции.
+                5.Удалить задачу из коллекции.
+                6.Показать элементы коллекции.
+                """
+        );
+    }
+
+    public static void main(String[] args) {
+        ArrayLists list = new ArrayLists();
+        Scanner console = new Scanner(System.in);
+        instruction();
+        int input = console.nextInt();
+
+        while (!(input ==0)) {
+            switch (input) {
+                case 1 -> {
+                    System.out.println("1.Добавить задачу в конец коллекции.");
+                    System.out.println("Введите тело задачи.");
+                    console.nextLine();
+                    String task1 = console.nextLine();
+                    list.addInList(task1);
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                case 2 -> {
+                    System.out.println("2.Добавить задачу в определенную позицию коллекции.");
+                    System.out.println("Введите тело задачи.");
+                    console.nextLine();
+                    String task2 = console.nextLine();
+                    System.out.println("Введите номер позиции задачи в коллекции. Длинна коллекции :" + list.sizeList());
+                    int index = console.nextInt();
+                    list.addInIndexList(index, task2);
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                case 3 -> {
+                    System.out.println("3.Получить индекс порядковой позиции задачи в коллекции.");
+                    System.out.println("Введите тело задачи.");
+                    console.nextLine();
+                    String task3 = console.nextLine();
+                    System.out.println(list.seurchIndexList(task3));
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                case 4 -> {
+                    System.out.println("4.Изменить задачу  в коллекции, в указанной позиции.");
+                    System.out.println("Введите тело задачи.");
+                    console.nextLine();
+                    String task4 = console.nextLine();
+                    System.out.println("Введите порядковый номер задачи которую нужно заменить :");
+                    int index2 = console.nextInt();
+                    list.setList(index2, task4);
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                case 5 -> {
+                    System.out.println("5.Удалить задачу из коллекции.");
+                    System.out.println("Порядковый номер задачи которую нужно удалить :");
+                    console.nextLine();
+                    int index3 = console.nextInt();
+                    list.removeList(index3);
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                case 6 -> {
+                    console.nextLine();
+                    list.printList();
+                    System.out.println("Операция выполнена.");
+                    System.out.println("Введите следущую команду :");
+                    input = console.nextInt();
+                }
+                default -> input = 0;
+            }
+        }
+    }
 }
