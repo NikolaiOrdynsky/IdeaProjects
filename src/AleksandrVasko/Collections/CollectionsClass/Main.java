ff9129f9e62f17e2829b2defad74447550602cb4
Nikolay <nikolai.ordynsky@yandex.ru>
2021-06-19 15:50:56 +0300

    19,06

--- src/AleksandrVasko/Collections/CollectionsClass/Main.java
+++ src/AleksandrVasko/Collections/CollectionsClass/Main.java
@@ -1,9 +1,4 @@
 package AleksandrVasko.Collections.CollectionsClass;
 
 public class Main {
-    public static void main(String[] args) {
-        Card card1 = new Card(Mast.BYBNI,Name.DAMA);
-        Card card2 = new Card(Mast.CHERVI,Name.KOROL);
-        System.out.println(card1.compareTo(card2));
-    }
 }
---
+++ src/AleksandrVasko/Collections/CollectionsClass/СompareToMetod/Main.java
@@ -0,0 +1,9 @@
+package AleksandrVasko.Collections.CollectionsClass.СompareToMetod;
+
+public class Main {
+    public static void main(String[] args) {
+        Card card1 = new Card(Mast.BYBNI, Name.DAMA);
+        Card card2 = new Card(Mast.CHERVI,Name.KOROL);
+        System.out.println(card1.compareTo(card2));
+    }
+}
