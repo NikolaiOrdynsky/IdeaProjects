5cd51cb10ccb0a9e6c12a795dd1b7f926b1cdccd
Nikolay <nikolai.ordynsky@yandex.ru>
2021-06-19 20:26:10 +0300

    19,06

--- src/AleksandrVasko/Collections/CollectionsClass/СompareToMetod/Card.java
+++ src/AleksandrVasko/Collections/CollectionsClass/СompareToMetod/Card.java
@@ -1,6 +1,8 @@
 package AleksandrVasko.Collections.CollectionsClass.СompareToMetod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Card implements Comparable<Card> {
@@ -20,14 +22,14 @@ public class Card implements Comparable<Card> {
         return name;
     }
 
-    @Override
     /* Сравнивает этот объект с указанным объектом для заказа.
-     Возвращает отрицательное целое число, ноль или положительное целое число,
-     поскольку этот объект меньше, равен или больше указанного объекта.*/
-    public int compareTo(Card card) { // в параметры передается объект того же типа что и у объекта у которого вызывается метод.
-        Name[] values = Name.values();//для сравнения будем использовать индексы очередности списка,
-                                      // поэтому очередность записи объектов в enum Name и есть условие старшинства объектов. Метод values() возвращает массив из enum.
-        List<Name> listName = Arrays.asList(values);//С помощью метода asList() преобразуем массив в коллекцию список.
+         Возвращает отрицательное целое число, ноль или положительное целое число,
+         поскольку этот объект меньше, равен или больше указанного объекта.*/
+    @Override
+    public int compareTo(Card card) {                    // в параметры передается объект того же типа что и у объекта у которого вызывается метод.
+        Name[] values = Name.values();                   //для сравнения будем использовать индексы очередности списка,
+                                                         // поэтому очередность записи объектов в enum Name и есть условие старшинства объектов. Метод values() возвращает массив из enum.
+        List<Name> listName = Arrays.asList(values);     //С помощью метода asList() преобразуем массив в коллекцию список.
 
 
         if (listName.indexOf(this.name) < listName.indexOf(card.getName())) {           // Сравниваем индекс поля name в списке listName , у текущего объекта,
@@ -41,5 +43,19 @@ public class Card implements Comparable<Card> {
                                                                                         // сравниваем с полем mast у объекта переданного в параметрах,  с помощью метода compareTo.
         }
         return 0; // возвращаем 0 если сравниваемые объекты равны и по полю name и по полю mast.
+    }
+
+    @Override
+    public String toString() {
+        return "Card " + mast + " " + name;
+
+    }
+    public static void printCard(ArrayList<Card> al) {
+        System.out.println("-------------------------------------------------");
+        int i = 0;
+        for (Card s : al) {
+            i++;
+            System.out.printf("%-30s %s", s.toString(), i % 4 == 0 ? "\n" : " ");
+        }
     }
 }
--- src/AleksandrVasko/Collections/CollectionsClass/СompareToMetod/Main.java
+++ src/AleksandrVasko/Collections/CollectionsClass/СompareToMetod/Main.java
@@ -1,9 +1,41 @@
 package AleksandrVasko.Collections.CollectionsClass.СompareToMetod;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 public class Main {
+
+
     public static void main(String[] args) {
         Card card1 = new Card(Mast.BYBNI, Name.DAMA);
-        Card card2 = new Card(Mast.CHERVI,Name.KOROL);
+        Card card2 = new Card(Mast.CHERVI, Name.KOROL);
         System.out.println(card1.compareTo(card2));
+
+        ArrayList<Card> al = new ArrayList<>();
+        Mast[] valueMast = Mast.values();//Создаем массив из элементов enum Mast.
+        Name[] valueName = Name.values();//Создаем массив из элементов enum Name.
+
+        //добавляем в ArrayList объекты класса Card.
+
+        for (Name name : valueName) {          // цикл по массиву valueName.
+            for (Mast mast : valueMast) {      // цикл по массиву valueMast.
+                al.add(new Card(mast, name));  //добавляем объект класса Card с текущими (mast,name) из циклов.
+            }
+        }
+
+
+        Card.printCard(al);  // Выводим на экран элементы из ArrayList
+
+        Collections.shuffle(al); // перемешать элементы в ArrayList.
+
+        Card.printCard(al);  // Выводим на экран элементы из ArrayList
+
+        al.sort(new ComparatorCard()); // Метод sort класса ArrayList сортирует элементы с помощью нашего компаратора.
+
+
     }
+
 }
+
