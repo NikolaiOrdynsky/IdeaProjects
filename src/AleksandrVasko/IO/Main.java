942bcdb08dfd35f419c835f10ac25b8b44c5847c
Nikolay <nikolai.ordynsky@yandex.ru>
2021-08-05 17:08:10 +0300

    5.08

--- src/AleksandrVasko/IO/Main.java
+++ src/AleksandrVasko/IO/Main.java
@@ -1,4 +1,21 @@
 package AleksandrVasko.IO;
 
+import AleksandrVasko.Collections.Map.TreeMap.PredmetGrade;
+import AleksandrVasko.Collections.Map.TreeMap.Student;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+        var map = Student.addStudent();
+       // Writer.bfWriteStudent(map,"Student.txt");
+       // Writer.bfWriteBankAccaunt("BankAccaunt");
+       // Writer.bfWriteFormatter("BankAccaunt2");
+       // Reader.FileReader("BankAccaunt");
+
+        Writer.objectWriter(map,"Object.bin");
+        Reader.objectReader("Object.bin");
+
+    }
 }
