09d1dfb81e401101c53172e78a7d36a8cb367600
NikolaiOrdynsky <80887986+NikolaiOrdynsky@users.noreply.github.com>
2021-10-12 17:00:25 +0300

    12.10

---
+++ src/PracticalTask/Замена_символов_в_строке/Main.java
@@ -0,0 +1,20 @@
+package PracticalTask.Замена_символов_в_строке;
+
+import java.util.Arrays;
+
+public class Main {
+    public static String correct(String string) {
+        String i = string.replace("1", "I");
+        String s = i.replace("5", "S");
+        String replace = s.replace("0", "O");
+        return replace;
+    }
+
+    public static String correctPro(String string) {
+
+        return string.replace("1", "I").replace("5", "S").replace("0", "O");
+    }
+    public static void main(String[] args) {
+        System.out.println(correctPro("R0BERT MERLE - THE DAY 0F THE D0LPH1N"));
+    }
+}
